565abef7e52b54903139f84d1dc4b57939a6ce0f
Panos Matsinopoulos <panagiotis@matsinopoulos.gr>
2023-06-18 22:49:46 +0300

    Type mapping

--- src/main/kotlin/Hello.kt
+++ src/main/kotlin/Hello.kt
@@ -3,4 +3,8 @@ fun main() {
     println(adversary.utterGreeting())
     val friendshipLevel = adversary.determineFriendshipLevel()
     println(friendshipLevel?.lowercase() ?: "It's complicated")
+    val hitPoints = adversary.hitPoints
+    println(hitPoints.javaClass)
+    println(hitPoints::class)
+    println("String"::class)
 }
--- src/main/kotlin/JHava.java
+++ src/main/kotlin/JHava.java
@@ -2,6 +2,8 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 public class JHava {
+    public int hitPoints = 52489112;
+
     @NotNull
     public String utterGreeting() {
         return "BLARGH";
